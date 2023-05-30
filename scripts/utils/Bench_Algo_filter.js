e274c1b675b1862b353b023574d267e07197505a
Eric BOUILLAUT <e.bouillaut@yahoo.fr>
2023-05-30 16:36:36 +0200

    opti algo while

--- scripts/utils/Bench_Algo_filter.js
+++ scripts/utils/Bench_Algo_filter.js
@@ -1,10 +1,12 @@
 const matchingRecipes = recipes.filter(function(recipe) {
+    let matchesSearchItem = true;
     const searchIngredients = recipe.ingredients.map(element => element.ingredient);
     const fullSearch = recipe.name + recipe.description + searchIngredients;
 
     if(currentSearch.length >=3){
         matchesSearchItem = fullSearch.toLowerCase().includes(currentSearch);
     }
+
 
     return matchesSearchItem;
 });
--- scripts/utils/Bench_Algo_while.js
+++ scripts/utils/Bench_Algo_while.js
@@ -1,66 +1,35 @@
-const matchingRecipes = [];
+const matchingRecipes = [];search
+const search = currentSearch.toLowerCase();
 let i = 0;
 while (i < recipes.length) {
     const recipe = recipes[i];
 
     let matchesSearchItem = false;
-    if(currentSearch.length < 3){
+    if(search.length < 3){
         matchesSearchItem = true;
     }else{
-        let element = recipe.name.toLowerCase();
+        const searchIngredients = [];
         let j = 0;
+        while (j < recipe.ingredients.length) {
+          searchIngredients.push(recipe.ingredients[j].ingredient);
+          j++;
+        }
+        const fullSearch = (recipe.name + recipe.description + searchIngredients).toLowerCase();
+
         let k = 0;
+        let l = 0;
       
-        while (j < element.length && k < currentSearch.length) {
-            if (element[j] === currentSearch[k]) {
-                k++;
+        while (k < fullSearch.length && l < search.length) {
+            if (fullSearch[j] === search[l].toLowerCase()) {
+                l++;
             } else {
-                k = 0;
+                l = 0;
             }
-            j++;
+            k++;
         }
 
-        if (k === currentSearch.length) {
+        if (l === search.length) {
             matchesSearchItem = true;
-        }else{
-            element = recipe.description.toLowerCase();
-            j = 0;
-            k = 0;
-      
-            while (j < element.length && k < currentSearch.length) {
-                if (element[j] === currentSearch[k]) {
-                    k++;
-                } else {
-                    k = 0;
-                }
-                j++;
-            }
-      
-            if (k === currentSearch.length) {
-                matchesSearchItem = true;
-            }else{
-                let j = 0;
-                while (j < recipe.ingredients.length) {
-
-                    element = recipe.ingredients[j].ingredient.toLowerCase();
-                    l = 0;
-                    k = 0;
-              
-                    while (l < element.length && k < currentSearch.length) {
-                        if (element[l] === currentSearch[k]) {
-                            k++;
-                        } else {
-                            k = 0;
-                        }
-                        l++;
-                    }
-              
-                    if (k === currentSearch.length) {
-                        matchesSearchItem = true;
-                    }
-                    j++;
-                }
-            }
         }
     }
     if (matchesSearchItem) {
